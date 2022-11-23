e8b7bbfb479a6f56f7e806a3a1b781d6e8ef9e58
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2022-11-23 19:51:51 +0300

    дописал программу до конца!

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,2 +1,13 @@
 class Game {
+    fun printResult(userChoice: String, gameChoice: String) {
+        val result: String
+        //Определить результат
+        if (userChoice == gameChoice) result = "Tie!"
+        else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Paper" && gameChoice == "Rock") ||
+            (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+        else result = "You lose!"
+        //Вывести результат
+        println("You chose $userChoice. I chose $gameChoice. $result")
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,43 @@
 fun main(args: Array<String>) {
     val option = arrayOf("камень","ножницы","бумага")
     val gameChoice = getGameChoice(option)
-
+    val userChoice = getUserChoice(option)
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParam: Array<String>) =
     optionsParam[(Math.random() * optionsParam.size).toInt()]
 
 fun getUserChoice(optionsParam: Array<String>): String {
-    //Запросить у пользователя его выбор
-    print("Please enter one of the following:")
-    for (item in optionsParam) print(" $item")
-    println(".")
-    val userInput = readLine()
-    return ""
+    var isValidChoice = false
+    var userChoice = ""
+    //Выполнять цикл, пока пользователь не введет допустимый вариант
+    while (!isValidChoice) {
+        //Запросить у пользователя его выбор
+        print("Please enter one of the following:")
+        for (item in optionsParam) print(" $item")
+        println(".")
+        //Прочитать пользовательский ввод
+        val userInput = readLine()
+        //Проверить пользовательский ввод
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+        //Если выбран недопустимый вариант, сообщить пользователю
+        if (!isValidChoice) println("You must enter a valid choice.")
+    }
+    return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+    //Определить результат
+    if (userChoice == gameChoice) result = "ничья!"
+    else if ((userChoice == "камень" && gameChoice == "ножницы")||
+    (userChoice == "бумага" && gameChoice == "камень") ||
+    (userChoice == "ножницы" && gameChoice == "бумага")) result = "Ты победил!"
+    else result = "Ты проиграл!"
+    //Вывести результат
+    println("Ты выбрал $userChoice. я выбрал $gameChoice. $result")
 }
